023ffbba3249b0e101774a711e7dfedc72683094
2fannad <tufan.nadjafi@gmail.com>
2016-04-20 21:36:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('hey fam. this is a way to get to know Tufan. Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
